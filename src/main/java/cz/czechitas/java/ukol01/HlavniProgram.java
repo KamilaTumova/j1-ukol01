7a47306f3cf82b4d372375ca4124d15a76e8dd18
Kamila <kaime31@gmail.com>
2023-02-18 20:56:57 +0100

    prasátko+domecek+sluníčko

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -8,16 +8,38 @@ public class HlavniProgram {
     public void start() {
         Turtle zofka = new Turtle();
 
-        //nakresliPrasatko(zofka);
-        // nakreslidomecek(zofka);
+//        nakresliPrasatko(zofka);
+//        nakreslidomecek(zofka);
+        nakresliSlunicko(zofka);
+
     }
+
+
+
+
 
     private static void nakreslidomecek(Turtle zofka) {
         nakresliStrizku(zofka);
         nakresliCtverec(zofka);
     }
+    private static void nakresliSlunicko(Turtle zofka) {
 
 
+
+        for (int i = 0; i < 9; i++) {
+            zofka.turnLeft(90);
+            zofka.move(40);
+            zofka.turnLeft(180);
+            zofka.move(40);
+            zofka.turnLeft(100);
+            zofka.turnRight(20);
+            zofka.move(20);
+            zofka.turnRight(20);
+            zofka.move(20);
+            zofka.turnRight(10);
+        }
+
+    }
     private static void nakresliPrasatko(Turtle zofka) {
         double b = 50;
         double c = 25;
